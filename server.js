e98136bb2c747d7537e16bd559dca074a5ab140c
Ubuntu <ubuntu@ip-172-31-7-61.ap-south-1.compute.internal>
2018-10-15 12:43:54 +0000

    modified server file

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const app = require('express')(),
       mongoClient = require('mongodb').MongoClient,
       url = "mongodb://admin:admin1@ds121163.mlab.com:21163/tjmaxx",
-      PORT= process.env.PORT||5000;
+      PORT= process.env.PORT||8080;
 const bodyParser = require('body-parser');
 var bodyparser=require('body-parser').json();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+});
+
 app.get('/',(req,res)=>{
   res.send('tjmax server');
 })
@@ -30,25 +38,26 @@ app.get('/getOrderData',(req,res)=>{
         }
      })
 })
-app.post('/returnInvoiceDetails',(req,res)=>
+app.post('/returnInvoiceDetails',bodyparser,(req,res)=>
 {
+console.log(req.body,"------------------------> data from req.body");
   var order_id="OR100001",
-      return_invoice_barcodeNo="883153834881",
-      barcode_no="883153834902",
+      return_invoice_barcodeNo="715676017633",
+      barcode_no="031655337913",
       barcode_type="UPC";
       var order_details = req.body;
-      console.log(req.body,"------------------------> data from req.body");
+      
      mongoClient.connect(url,(err,client)=>{
          if(err) throw err;
          else{
            const db = client.db('tjmaxx');
-           db.collection('returnInvoiceDetails').insert({order_id:order_id,return_invoice_barcodeNo:return_invoice_barcodeNo,barcode_no:barcode_no,barcode_type:barcode_type,order_details:order_details}),(function(err,result) {
+           db.collection('returnInvoiceDetails').update({order_id:order_id},{$set:{order_details:order_details}}),(function(err,result) {
                client.close();
 
              })
-         }
+        }
      })
- res.send("returnInvoiceDetails added successfully")
+ res.send("response from server");
 
 })
 
